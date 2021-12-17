5570fd0b673ec93ff20accb57ab280ae5fc9ec6d
Saksham Maurya <saksham.maurya2709@gmail.com>
2021-12-17 19:51:25 +0530

    modified

--- VM_Instance/Variables.tf
+++ VM_Instance/Variables.tf
@@ -38,7 +38,7 @@ variable "publickeypath" {
 }
 variable "name " {
     type = string
-    default = "terraforminstance1"
+    default = "My-First-Instance"
 }
 variable "machine_type" {
     type = string
